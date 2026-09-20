239b380659dea6716e03d49f95f77c89649f76aa
Tanish428 <tanishmistry007@gmail.com>
2026-09-20 16:20:22 +0530

    Created DeviceCategories controller and pure HTML views

---
+++ SmartDeviceMatch/Controllers/DeviceCategoriesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartDeviceMatch.Data;
+using SmartDeviceMatch.Models;
+
+namespace SmartDeviceMatch.Controllers
+{
+    public class DeviceCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        
+        public DeviceCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+        public IActionResult Index()
+        {
+            
+            var categoriesList = _context.DeviceCategories.ToList();
+
+            
+            return View(categoriesList);
+        }
+
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Create(DeviceCategory newCategory)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.DeviceCategories.Add(newCategory);
+                _context.SaveChanges();                   
+
+                return RedirectToAction("Index");         
+            }
+
+           
+            return View(newCategory);
+        }
+    }
+}
